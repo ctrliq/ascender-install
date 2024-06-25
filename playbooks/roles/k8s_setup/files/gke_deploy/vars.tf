6c7e5dc66117db5944fcb43995cf5f25dc081a8c
Michael Ford <michaelford85@gmail.com>
2024-06-25 05:58:52 +0000

    gke cluster creation

---
+++ playbooks/roles/k8s_setup/files/gke_deploy/vars.tf
@@ -0,0 +1,56 @@
+/*
+variable "use_existing_project" {
+  description = "Boolean to decide whether to use an existing project or create a new one."
+  type        = bool
+  default     = false
+}
+*/
+
+variable "project_id" {
+  description = "The ID of the existing project to use. Only required if use_existing_project is true."
+  type        = string
+  default     = "dummy_project"
+}
+
+/*
+variable "billing_account" {
+  description = "The ID of the billing account to associate with the project."
+  type        = string
+}
+*/
+
+variable "zone" {
+  description = "The zone to deploy the GKE cluster."
+  type        = string
+  default     = "us-central1-a"
+}
+
+variable "gke_cluster_name" {
+  description = "The name of the GKE cluster"
+  type        = string
+  default     = "ascender-gke-cluster"
+}
+
+variable "num_nodes" {
+  description = "The number of nodes in the node pool."
+  type        = number
+  default     = 3
+}
+
+variable "gcloud_vm_size" {
+  description = "The size of the instance (machine type) for the worker nodes."
+  type        = string
+  default     = "n1-standard-1"
+}
+
+variable "volume_size" {
+  description = "The size of the boot disk for the worker nodes, in GB."
+  type        = number
+  default     = 100
+}
+
+variable "home_dir" {
+  description = "Home directory path"
+  type        = string
+  default     = "/home/default"
+}
